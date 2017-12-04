f56b1d8a7bea39d4970d41c1d11850ee0e1a64fa
Sjoerd Brauer <s.brauer@student.fontys.nl>
2017-12-04 10:22:38 +0100

    added raw version of oauth workshop

---
+++ demo-oauth-client/src/main/java/com/example/demooauthclient/DemoOauthClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.demooauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOauthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOauthClientApplication.class, args);
+	}
+}
---
+++ demo-oauth-client/src/main/java/com/example/demooauthclient/OauthConfig.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demooauthclient;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author Gebruiker
+ */
+@EnableOAuth2Sso
+@Configuration
+public class OauthConfig extends WebSecurityConfigurerAdapter{
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+    }
+}
